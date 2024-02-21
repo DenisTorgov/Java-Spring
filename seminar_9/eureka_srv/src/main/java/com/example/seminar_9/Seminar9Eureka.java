8dda6d0c1ae9dd4fa1fc5999d3ec94066446c212
Denis.Torgov <deniska-3000@bk.ru>
2024-02-21 22:52:11 +0300

    Task 9 update

--- seminar_9/eureka_srv/src/main/java/com/example/seminar_9/Seminar9Eureka.java
+++ seminar_9/eureka_srv/src/main/java/com/example/seminar_9/Seminar9Eureka.java
@@ -3,9 +3,6 @@ package com.example.seminar_9;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-//import org.springframework.cloud.gateway.route.RouteLocator;
-//import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-//import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -14,12 +11,4 @@ public class Seminar9Eureka {
 	public static void main(String[] args) {
 		SpringApplication.run(Seminar9Eureka.class, args);
 	}
-
-//	@Bean
-//	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-//		return builder.routes()
-//				.route("TaskMicroservice",r->r.path("/serviceTasks/**")
-//						.uri("http://localhost:8081/")).build();
-//	}
-
 }
